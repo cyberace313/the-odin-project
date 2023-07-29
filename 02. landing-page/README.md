efeb5e04653135a4a19d69816fe535822944925e
cyberace313 <zafer.arifi290@gmail.com>
2023-07-29 13:36:00 +0430

    Uploaded the first three projects

---
+++ 02. landing-page/README.md
@@ -0,0 +1,2 @@
+# landing-page
+This is one of the projects from the Odin Project.
---
+++ 03. rock-paper-scissors/script.js
@@ -0,0 +1,65 @@
+game();
+
+function round(playerSelection, computerSelection) {
+  let message = "";
+  let win = 0;
+  let lose = 0;
+  if (playerSelection == "rock" && computerSelection == "rock") {
+    message = "Stalmate! Repeat!";
+    alert(message);
+    return message;
+  } else if (playerSelection == "rock" && computerSelection == "paper") {
+    message = "You Lose! Paper beats rock.";
+    alert(message);
+    lose += 1;
+    return message;
+  } else if (playerSelection == "rock" && computerSelection == "scissor") {
+    message = "You Win! Rock beats scissor";
+    alert(message);
+    win += 1;
+    return message;
+  } else if (playerSelection == "paper" && computerSelection == "rock") {
+    message = "You win! Paper beats rock.";
+    alert(message);
+    return message;
+  } else if (playerSelection == "paper" && computerSelection == "paper") {
+    message = "Stalemate! Repeat!";
+    alert(message);
+    return message;
+  } else if (playerSelection == "paper" && computerSelection == "scissor") {
+    message = "You lose! Scissor beats paper";
+    alert(message);
+    return message;
+  } else if (playerSelection == "scissor" && computerSelection == "rock") {
+    message = "You lose! Rock beats scissor";
+    alert(message);
+    return message;
+  } else if (playerSelection == "scissor" && computerSelection == "paper") {
+    message = "You win! Scissor beats paper";
+    alert(message);
+    return message;
+  } else if (playerSelection == "scissor" && computerSelection == "scissor") {
+    message = "Stalemate! Repeat";
+    alert(message);
+    return message;
+  }
+}
+
+function game() {
+  for (let i = 1; i < 5; i++) {
+    let userChoice = prompt("Please input an option from rock-paper-scissor");
+    let finalChoice = userChoice.toLowerCase();
+
+    function getComputerChoice() {
+      let choicesArray = ["rock", "paper", "scissor"];
+      let randomIndex = Math.floor(Math.random() * choicesArray.length);
+      let choice = choicesArray[randomIndex];
+      return choice;
+    }
+
+    let playerSelection = finalChoice;
+    const computerSelection = getComputerChoice();
+
+    round(playerSelection, computerSelection);
+  }
+}
