e0cd4b47d9c02626e02d416fce435658fa24d980
Zafer Arifi <zafer.arifi290@gmail.com>
2023-09-10 14:33:39 +0430

    Add the grid size range

--- 04. etch-a-sketch/script.js
+++ 04. etch-a-sketch/script.js
@@ -5,9 +5,18 @@ const pen = document.getElementById("pen");
 const eraser = document.getElementById("eraser");
 const colorPicker = document.getElementById("color-picker");
 const clear = document.getElementById("clear");
+const range = document.getElementById("range");
+const dimensions = document.querySelector(".dimensions");
+
+dimensions.textContent = `${range.value}x${range.value}`;
+console.log(range.value);
+grid.style.gridTemplateColumns = `repeat(${range.value}, 1fr)`;
 
 let penSelected = false;
 let eraserSelected = false;
+
+// grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+start();
 
 pen.addEventListener("click", () => {
   pen.classList.add("selected");
@@ -24,23 +33,19 @@ eraser.addEventListener("click", () => {
   penSelected = false;
 });
 
-colorPicker.addEventListener("click", () => {
-  colorPicker.classList.add("selected");
-  eraser.classList.remove("selected");
-  pen.classList.remove("selected");
-});
+function start() {
+  for (let i = 0; i < range.value ** 2; i++) {
+    let boxDiv = document.createElement("div"); // Create a new div element for each iteration
+    boxDiv.classList.add("box");
 
-for (let i = 0; i < 256; i++) {
-  let boxDiv = document.createElement("div"); // Create a new div element for each iteration
-  boxDiv.classList.add("box");
+    boxDiv.addEventListener("click", () => {
+      if (penSelected) {
+        boxDiv.classList.add("black-dot");
+      } else if (eraserSelected) {
+        boxDiv.classList.remove("black-dot");
+      }
+    });
 
-  boxDiv.addEventListener("click", () => {
-    if (penSelected) {
-      boxDiv.classList.add("black-dot");
-    } else if (eraserSelected) {
-      boxDiv.classList.remove("black-dot");
-    }
-  });
-
-  grid.appendChild(boxDiv);
+    grid.appendChild(boxDiv);
+  }
 }
