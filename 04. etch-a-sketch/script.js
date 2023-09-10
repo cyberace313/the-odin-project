de8919ab7eac97f61bc5e666585a2264ab9310df
Zafer Arifi <zafer.arifi290@gmail.com>
2023-09-10 11:51:44 +0430

    Design the interface and add the button functionalities

--- 04. etch-a-sketch/script.js
+++ 04. etch-a-sketch/script.js
@@ -1,10 +1,46 @@
-const gridContainer = document.querySelector(".grid-container");
+const grid = document.querySelector(".grid");
 
-let boxDiv = document.createElement("div");
-boxDiv.classList.add("box");
+// Toolbar Buttons:
+const pen = document.getElementById("pen");
+const eraser = document.getElementById("eraser");
+const colorPicker = document.getElementById("color-picker");
+const clear = document.getElementById("clear");
+
+let penSelected = false;
+let eraserSelected = false;
+
+pen.addEventListener("click", () => {
+  pen.classList.add("selected");
+  eraser.classList.remove("selected");
+  colorPicker.classList.remove("selected");
+  penSelected = true;
+});
+
+eraser.addEventListener("click", () => {
+  eraser.classList.add("selected");
+  pen.classList.remove("selected");
+  colorPicker.classList.remove("selected");
+  eraserSelected = true;
+  penSelected = false;
+});
+
+colorPicker.addEventListener("click", () => {
+  colorPicker.classList.add("selected");
+  eraser.classList.remove("selected");
+  pen.classList.remove("selected");
+});
 
 for (let i = 0; i < 256; i++) {
-  gridContainer.appendChild(boxDiv.cloneNode(true));
-}
+  let boxDiv = document.createElement("div"); // Create a new div element for each iteration
+  boxDiv.classList.add("box");
 
-boxDiv.addEventListener();
+  boxDiv.addEventListener("click", () => {
+    if (penSelected) {
+      boxDiv.classList.add("black-dot");
+    } else if (eraserSelected) {
+      boxDiv.classList.remove("black-dot");
+    }
+  });
+
+  grid.appendChild(boxDiv);
+}
